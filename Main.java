94d08920a7de2af99189d1e7fa33203263d4908e
jazadash04 <31344694-jazadash04@users.noreply.replit.com>
2024-01-30 19:05:46 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+//This program prints out the following: "Welcome to Java", "Welcome to Computer Science", and "Programming is fun"
+
+class Main {
+  public static void main(String[] args) {
+    //Print "Welcome to Java" 
+    System.out.println("Welcome to Java");
+    //Print "Welcome to Computer Science"
+    System.out.println("Welcome to Computer Science");
+    //Print "Programming is fun"
+    System.out.println("Programming is fun");
+  }
+}
